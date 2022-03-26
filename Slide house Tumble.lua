e49514e219701445deff64e92bf08b86c22dac16
TuxWasHere <77133367+tuxudoman356@users.noreply.github.com>
2022-03-26 16:13:10 -0400

    Slide house tumble script
    Script for slide house tumble

---
+++ Slide house Tumble.lua
@@ -0,0 +1,21 @@
+-- slide house tumble by Tux#9417
+
+
+getgenv().autotp = true
+
+
+while autotp == true do
+    wait(3)
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.EndStuff.SpawnLocation, 0)
+    wait(3)
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Spawn.SpawnLocation, 0)
+    wait(3) 
+end
+
+ 
+
+
+
+
+
+
